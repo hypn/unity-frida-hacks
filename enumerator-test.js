257de437a1479ed2d5cc0d0479f401fefd7303bc
Ross Simpson <ross.simpson@gmail.com>
2020-04-19 12:53:06 +0200

    Corrected class for test file

--- enumerator-test.js
+++ enumerator-test.js
@@ -1,7 +1,7 @@
 import Enumerator from './enumerator.js'
 
 // mono class we want to enumerate
-var takeDamage = Enumerator.enumerateClass('RoadRenderer');
+var takeDamage = Enumerator.enumerateClass('TakeDamage');
 
 // print it out
 Enumerator.prettyPrint(takeDamage);
